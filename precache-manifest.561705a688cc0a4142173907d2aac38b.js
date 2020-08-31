33971fe4fba2e4906d7472d2f359c01f5d0c373b
cazdemun <cplu1509@gmail.com>
2020-08-30 23:11:40 -0500

    Updates

---
+++ precache-manifest.561705a688cc0a4142173907d2aac38b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d29cad38ce6d7bfcf246e2f137f064d8",
+    "url": "/teachers4teachers-poc/index.html"
+  },
+  {
+    "revision": "2abf3e0bd09eb014a61b",
+    "url": "/teachers4teachers-poc/static/css/2.847e01e0.chunk.css"
+  },
+  {
+    "revision": "f6f54d4a7e917c06bbbb",
+    "url": "/teachers4teachers-poc/static/css/main.fb650395.chunk.css"
+  },
+  {
+    "revision": "2abf3e0bd09eb014a61b",
+    "url": "/teachers4teachers-poc/static/js/2.cc9a8fa4.chunk.js"
+  },
+  {
+    "revision": "4fdb92f5a0a11be5d79ae606cb253c2b",
+    "url": "/teachers4teachers-poc/static/js/2.cc9a8fa4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f6f54d4a7e917c06bbbb",
+    "url": "/teachers4teachers-poc/static/js/main.0846ad97.chunk.js"
+  },
+  {
+    "revision": "437f2778e5e24b6caa7f",
+    "url": "/teachers4teachers-poc/static/js/runtime-main.7fbab068.js"
+  }
+]);
